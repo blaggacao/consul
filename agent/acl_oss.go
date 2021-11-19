0c421399b073e74f2c610cdee9b3367a657fee4d
Daniel Upton <daniel@floppy.co>
2021-11-19 12:11:23 +0000

    agent: support filtered-by-acls header in `/services` endpoint

--- agent/acl_oss.go
+++ agent/acl_oss.go
@@ -6,8 +6,13 @@ import (
 	"github.com/hashicorp/serf/serf"
 
 	"github.com/hashicorp/consul/acl"
+	"github.com/hashicorp/consul/api"
 )
 
 func serfMemberFillAuthzContext(m *serf.Member, ctx *acl.AuthorizerContext) {
+	// no-op
+}
+
+func agentServiceFillAuthzContext(s *api.AgentService, ctx *acl.AuthorizerContext) {
 	// no-op
 }
